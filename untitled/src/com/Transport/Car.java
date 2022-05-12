0118af9f3163d06232e0ba0674dc70f1c7f27c9c
YuraYuraYura <lyn81184@gmail.com>
2022-05-12 12:59:28 +0300

    wrk

--- untitled/src/com/Transport/Car.java
+++ untitled/src/com/Transport/Car.java
@@ -1,4 +1,11 @@
 package com.Transport;
 
 public class Car {
+    public static void main(String[] args) {
+      /*  rgfgrwr
+
+                werd
+                fw
+                        r*/
+    }
 }
