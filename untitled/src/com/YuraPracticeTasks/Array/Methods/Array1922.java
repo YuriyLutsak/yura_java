b202ab4645c01f6b3dd2e74b8031de25413eec7a
YuraYuraYura <lyn81184@gmail.com>
2022-06-18 11:45:23 +0300

    array1922forexam

---
+++ untitled/src/com/YuraPracticeTasks/Array/Methods/Array1922.java
@@ -0,0 +1,49 @@
+package com.YuraPracticeTasks.Array.Methods;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Задание 19.22
+ * Напишите метод, который принимает два параметра:
+ * <p>
+ * массив целых чисел М и еще одно целое число Х.
+ * Метод возвращает количество ячеек массива М, содержащих значение Х (число появлений Х в М).
+ */
+public class Array1922 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("size of array: ");
+        int[] arr = new int[scanner.nextInt()];
+
+        System.out.println(" enter one-digital value: ");
+        int value = scanner.nextInt();
+
+        System.out.println(returningValueFromArray(arr, value));
+
+    }
+
+    public static int returningValueFromArray(int[] arr, int value) {
+
+        int counter = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            arr[i] = (int) (Math.random() * 10);
+            // System.out.println(i + ": " + arr[i] + " suka - psihanul)  ");
+
+            if (arr[i] == value) {
+                counter++;
+                //   System.out.print(i + ": " + arr[i] + " ->     ");
+                //return counter;
+            }
+        }
+//        System.out.println();
+//        System.out.print(" counter is:   " + counter + "      ");
+
+        return counter;
+    }
+}
+
+
